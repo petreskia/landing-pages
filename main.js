6f2238f6822084353b1994d6963a0204de7646e5
Andrej Petreski <petreskia@hotmail.com>
2024-12-30 18:13:42 +0100

    Test on netlify

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".nav-link");
 
-  // Get the current path; treat root ("/") as "index.html"
+  // Get the current path, consider root as index.html
   const currentPath =
-    window.location.pathname === "/"
+    window.location.pathname === "/" ||
+    window.location.pathname === "/index.html"
       ? "index.html"
       : window.location.pathname.split("/").pop();
 
   // Highlight the active link
   navLinks.forEach((link) => {
-    const href = link.getAttribute("href").replace("./", ""); // Normalize href
+    const href = link.getAttribute("href").replace("./", ""); // Normalize href (strip leading ./)
     if (href === currentPath) {
       link.classList.add("active");
-      link.classList.remove("text-white-50"); // Optional: Remove inactive styles
     } else {
       link.classList.remove("active");
-      link.classList.add("text-white-50"); // Optional: Add inactive styles
     }
   });
 });
