2ab0dcdade28c884c81de876916bb7d20ee7e032
Andrej Petreski <petreskia@hotmail.com>
2025-01-30 23:49:04 +0100

    fix the intro text to be in loop

---
+++ main.js
@@ -0,0 +1,18 @@
+// JavaScript for dynamic text change (add this to a <script> tag or a JavaScript file)
+const changingText = document.getElementById("changingText");
+const phrases = [
+  "with people that care",
+  "with Experts",
+  "for Growth",
+  "with Passion",
+  "with Us",
+];
+let index = 0;
+
+// Set initial value to "with people that care"
+changingText.textContent = phrases[0];
+
+setInterval(() => {
+  changingText.textContent = phrases[index];
+  index = (index + 1) % phrases.length;
+}, 2000); // 3 seconds interval for each change
